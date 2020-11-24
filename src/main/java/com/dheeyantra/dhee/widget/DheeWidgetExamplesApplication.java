751520b2c22ce12a384a261575c44c2b9d7f1f69
soumyadip <soumyadip@dheeyantra.com>
2020-11-24 15:00:40 +0530

    initial commit

---
+++ src/main/java/com/dheeyantra/dhee/widget/DheeWidgetExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.dheeyantra.dhee.widget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DheeWidgetExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DheeWidgetExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dheeyantra/dhee/widget/DheeWidgetExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dheeyantra.dhee.widget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DheeWidgetExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
